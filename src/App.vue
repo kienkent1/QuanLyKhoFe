31caab30432912a76ce2e4c2fe7b7263cc99c131
ThangDz2 <chauquangthang135@gmail.com>
2025-10-01 13:55:31 +0700

    #2-QThang-SlideBar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-
+import Sidebar from './components/sidebar/Sidebar.vue';
 
 </script>
 
 <template>
-  <router-link to="/">Home</router-link> 
-
+  <Sidebar></Sidebar>
   <main>
     <router-view/>
   </main>
