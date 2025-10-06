484fec8137494cc5b38cd8813571ef2e2c0cd70a
ThangDz2 <chauquangthang135@gmail.com>
2025-10-06 11:13:27 +0700

    #16-Qthang-404

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
 // import Sidebar from './components/sidebar/Sidebar.vue';
-import Register from './components/Register/Register.vue';
+// import Register from './components/Register/Register.vue';
+import NotFound from './components/NotFound/NotFound.vue';
 </script>
 
 <template>
-  <Register></Register>
-  <!-- <Sidebar></Sidebar> -->
   <main>
-    <router-view/>
+    <NotFound />
+    <router-view />
   </main>
-
 </template>
+
 
 
