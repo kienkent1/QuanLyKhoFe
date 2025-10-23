e3a2c1f93ddc3f416d639c6f43631f6399684bd9
duydk27 <duydktb00841@fpt.edu.vn>
2025-10-23 14:14:10 +0700

    Done form nhân viên

--- src/components/Employee/User.js
+++ src/components/Employee/User.js
@@ -7,3 +7,15 @@ export const userData = async (query = {}) => {
   const data = await  api.getApi(controller, {requiresAuth: true}, query)
   return data.data.data
 } 
+
+export const createUser = async (data) => {
+  return await api.postApi(controller, data, {requiresAuth: true});
+}
+
+export const updateUser = async (id, data) => {
+  return await api.putApi(`${controller}/${id}`, data, {requiresAuth: true});
+}
+
+export const deleteUser = async (id) => {
+  return await api.deleteApi(`${controller}/${id}`, {requiresAuth: true})
+}
